90d42c23cfe356283825e9822ff9fcc15a93c948
Angelina <pretzel318@gmail.com>
2025-06-08 22:17:57 +0200

    Правки по типам пропсов Input

--- src/modules/auth/atoms/Input/index.tsx
+++ src/modules/auth/atoms/Input/index.tsx
@@ -1,5 +1,21 @@
 import styles from './Input.module.scss'
+import { ChangeEvent } from 'react'
 
-export const Input = () => {
-  return <input className={styles.input} />
+interface InputProps {
+  type: string
+  value: string
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void
+  placeholder?: string
+}
+
+export const Input = ({ type, value, onChange, placeholder }: InputProps) => {
+  return (
+    <input
+      className={styles.input}
+      type={type}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+    />
+  )
 }
