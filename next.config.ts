6b97414993716972e0a3f0b2ec904420fb664cb3
Angelina <pretzel318@gmail.com>
2025-06-12 00:10:31 +0200

    Добавлены GITHUB_ID, GITHUB_SECRET в nextConfig

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  env: {
+    GITHUB_ID: process.env.AUTH_GUTHUB_ID,
+    GITHUB_SECRET: process.env.AUTH_GUTHUB_SECRET,
+  },
   images: {
     domains: ['avatars.githubusercontent.com'],
   },
