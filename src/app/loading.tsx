da8c59913afadc302e625460180751b383169fef
Angelina <pretzel318@gmail.com>
2025-07-04 23:34:04 +0200

    Добавлен лоадер

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+import Loader from '@/shared/atoms/Loader'
+
+const Loading = () => {
+  return <Loader />
+}
+
+export default Loading
---
+++ src/shared/atoms/Loader/index.tsx
@@ -0,0 +1,11 @@
+import styles from './Loader.module.scss'
+
+const Loader = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.loader}></div>
+    </section>
+  )
+}
+
+export default Loader
