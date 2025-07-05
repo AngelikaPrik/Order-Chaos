c221fca457154a3ec0f3e023b32a9eca69792bae
Angelina <pretzel318@gmail.com>
2025-07-05 14:52:48 +0200

    Добавлен базовый template для создания переходов между страницами

---
+++ src/app/template.tsx
@@ -0,0 +1,24 @@
+'use client'
+import { AnimatePresence, motion } from 'framer-motion'
+import { usePathname } from 'next/navigation'
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname()
+  return (
+    // eslint-disable-next-line prettier/prettier
+    <AnimatePresence mode="popLayout">
+      <motion.main
+        key={pathname}
+        initial={{ opacity: 0, filter: 'blur(5px)' }}
+        animate={{
+          opacity: 1,
+          filter: 'blur(0px)',
+        }}
+        exit={{ opacity: 0, filter: 'blur(5px)' }}
+        transition={{ duration: 0.6, ease: 'easeInOut' }}
+      >
+        {children}
+      </motion.main>
+    </AnimatePresence>
+  )
+}
